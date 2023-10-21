b1e49d1a69c0d2c6585fc2d291d7a28ef72d6c69
Fredy ⚙️ <taboutsafredy@gmail.com>
2023-10-21 18:39:42 +0200

    Update credentials.py and remove credentials data.

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-TOKEN_BOT = "6434513002:AAG9MY61-egM0z4K0Js2h9s5InjrT437SVQ"
-USERNAME_BOT = "@setAvatarBot"
-BOT_URL = "https://t.me/setAvatarBot"
+TOKEN_BOT = "token"
+USERNAME_BOT = "username"
+BOT_URL = "url"
